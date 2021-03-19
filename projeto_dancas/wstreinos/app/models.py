8a3e67881d9226f0318bdea0a69f4695ea47e00f
= <=>
2021-03-18 21:30:32 -0300

    alterado model

--- projeto_dancas/wstreinos/app/models.py
+++ projeto_dancas/wstreinos/app/models.py
@@ -5,6 +5,7 @@ class Usuario(models.Model):
     login = models.CharField('login', max_length=100, null=False, blank=False, unique=True)
     senha = models.CharField('senha', max_length=100, null=False, blank=False)
     bloqueado = models.BooleanField('bloqueado', default=False)
+    professor = models.BooleanField('professor', default=False)
 
 class Aparelho(models.Model):
     nome = models.CharField('nome', max_length=255, unique=True)
