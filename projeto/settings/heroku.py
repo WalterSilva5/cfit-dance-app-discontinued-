692f35d5827dffd09d0a8d7da859935a9d932e43
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-11 13:06:43 -0300

    teste deploy

--- projeto/settings/heroku.py
+++ projeto/settings/heroku.py
@@ -1,15 +1,11 @@
 import environ
-
 from projeto.settings.base import *
-
 env = environ.Env()
-
 DEBUG = env.bool("DEBUG", False)
-
 SECRET_KEY = env("SECRET_KEY")
-
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-
+DISABLE_COLLECTSTATIC = env.list("DISABLE_COLLECTSTATIC")
 DATABASES = {
     "default": env.db(),
 }
+
