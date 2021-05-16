e4d85ed1300f190a3cc68c66a6256d4acf79a0dc
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-16 10:16:09 -0300

    alterado index

--- staticfiles/cfit/js/componentes/CfitButton.js
+++
@@ -1,9 +0,0 @@
-const CfitButton = {
-    template: `
-        <button 
-            class=" font-weight-bold btn btn-lg w3-orange"
-        ><slot>BUTTON</slot></button>
-    `,
-};
-
-export default CfitButton;
