3cde042427e9d4732231283384fda2ff14187614
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-17 09:19:28 -0300

    static files

--- staticfiles/cfit/js/componentes/WsiLogin.js
+++ staticfiles/cfit/js/componentes/WsiLogin.js
@@ -1,26 +1,26 @@
 const WsiLogin = {
     template: `
     <div class="row d-flex justify-content-center text-center">
-        <div class="col-12 w3-black py-4 rounded" style="border: 3px solid orange;">
-            <form id="form-login" autocomplete="off">
-                <label for="login_usuario" class="p-0 float-left">Usuario <span class="text-danger" id="login_usuario_invalid" style="display:none">* CAMPO OBRIGATORIO!</span></label>
+        <div class="col-12 w3-black py-4 rounded border w3-border-deep-orange">
+            <form id="form-login" autocomplete="off" class="container">
+                <label for="login_usuario" class="p-0 float-left font-weight-bold">Usuario <span class="text-danger" id="login_usuario_invalid" style="display:none">* CAMPO OBRIGATORIO!</span></label>
                 <input class="form-control" name="login_usuario" id="login_usuario" type="text">
                 <div class="my-2">
-                    <label for="login_senha" class="p-0 float-left">Senha <span class="text-danger" id="login_senha_invalid" style="display:none">* CAMPO OBRIGATORIO</span></label>
+                    <label for="login_senha" class="p-0 float-left font-weight-bold"><b>Senha</b><span class="text-danger" id="login_senha_invalid" style="display:none">* CAMPO OBRIGATORIO</span></label>
                     <input class="form-control" name="login_senha" id="login_senha" type="password">
                 </div>
                 <div class="alert alert-danger my-2" id="login_alert" role="alert" style="display: none;">
                     <h3 id="login_alert_msg"></h3>
                 </div>
             </form>
-            <wsi_button class="w3-orange mx-1 mt-4 w3-xlarge" onclick="efetuar_login()">LOGIN</wsi_button>
-            <div class="mt-3">
+            <wsi_button class="mx-1 mt-4 w3-xlarge" onclick="efetuar_login()"  style="filter: sepia(15%);">LOGIN</wsi_button>
+            <div class="mt-5">
                 <div class="col-12 d-flex justify-content-center">
                     <div>
                         Não cadastrado?
-                        <wsi_button class="w3-red text-light" data-target="#modal_cadastro" data-toggle="modal">
-                            CADASTRE-SE
-                        </wsi_button>
+                        <button data-target="#modal_cadastro" data-toggle="modal" id="btn_cadastre_se" class="btn w3-blue">
+                            <b class="font-weight-bold">CADASTRE-SE</b>
+                        </button>
                     </div>
                 </div>
             </div>
--- staticfiles/cfit/js/index/index_deslogado.js
+++ staticfiles/cfit/js/index/index_deslogado.js
@@ -1,3 +1,8 @@
+$(document).ready(function (e) {
+    $("#btn_cadastre_se").on("click", function () {
+        $("#modal_login").modal("toggle")
+    })
+})
 erro_cadastro = function (mensagem) {
     $("#alert_cadastro_msg").text(mensagem)
     $("#alert_cadastro").show()
@@ -16,9 +21,15 @@ cadastrar_usuario = function () {
                 cadastro_senha,
             },
             success: function (result) {
-                $("#cadastro_alert_sucesso").show()
-                $("#alert_cadastro").hide()
-                $("#form_cadastro").trigger("reset")
+                if(result == "cadastro_efetuado"){
+                    $("#cadastro_alert_sucesso").show()
+                    $("#alert_cadastro").hide()
+                    $("#form_cadastro").trigger("reset")
+                }else if(result == "ja_cadastrado"){
+                    erro_cadastro("USUARIO JÁ ESTÁ CADASTRADO!")
+                }else if(result == "usuario_menor_que_4"){
+                    erro_cadastro("NOME DE USUARIO DEVE TER PELO MENOS 4 LETRAS!")
+                }
             },
             error: function (result) {
                 console.log(result)
@@ -67,11 +78,18 @@ efetuar_login = function () {
             },
             success: function (result) {
                 if (result == "ok") {
+                    $("#form-login").trigger("reset");
                     window.location.replace("/home")
+                } else if (result == "nao_cadastrado") {
+                    login_erro("USUARIO NÃO CADASTRADO")
+                } else if (result = "usuario_menor_que_4") {
+                    login_erro("USUARIO INVALIDO")
+                } else {
+                    console.log(result)
                 }
             },
             error: function (result) {
-                erro_login(result)
+                login_erro(result.statusText)
                 console.log(result)
             }
         });
