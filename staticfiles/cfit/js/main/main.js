0e1b29c1d74f49c43ec74be31987295a641224fc
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-18 18:19:47 -0300

    alterado readi

--- staticfiles/cfit/js/main/main.js
+++ staticfiles/cfit/js/main/main.js
@@ -1,17 +1,16 @@
 import comps from "../componentes/componentes.js";
-const app = Vue.createApp({})
-app.component('wsi_button', comps.WsiButton)
-app.component('wsi_modal', comps.WsiModal)
-app.component('wsi_login', comps.WsiLogin)
-app.component('wsi_card_playlist', comps.WsiCardComps.WsiCardPlaylist)
-app.component('wsi_input', comps.WsiInputComps.WsiInput)
-app.component('wsi_input_group_left', comps.WsiInputComps.WsiInputGroupLeft)
-app.component('wsi_video_player_iframe', comps.WsiVideoPlayerIframe)
-app.mount("#app");
-
-$(document).ready(
-    $("#ano").text(new Date().getFullYear())
-)
+$(document).ready(function () {
+    const app = Vue.createApp({})
+    app.component('wsi_button', comps.WsiButton)
+    app.component('wsi_modal', comps.WsiModal)
+    app.component('wsi_login', comps.WsiLogin)
+    app.component('wsi_card_playlist', comps.WsiCardComps.WsiCardPlaylist)
+    app.component('wsi_input', comps.WsiInputComps.WsiInput)
+    app.component('wsi_input_group_left', comps.WsiInputComps.WsiInputGroupLeft)
+    app.component('wsi_video_player_iframe', comps.WsiVideoPlayerIframe)
+    app.mount("#app");
+    $("#ano").text(new Date().getFullYear());
+})
 $("#botao_opcoes").click(function () {
     $("#painel").fadeToggle();
 });
