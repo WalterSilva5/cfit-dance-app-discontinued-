ab4c567f21e8250082a5317349ea192934fc80f2
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-19 13:21:24 -0300

    alterado

--- staticfiles/cfit/js/componentes/WsiCard.js
+++ staticfiles/cfit/js/componentes/WsiCard.js
@@ -1,10 +1,10 @@
 const WsiCardPlaylist = {
     template: `
-    <div class="col-xs-4 hvr hvr-float-shadow " style="width:100%;max-width:none;padding:0;">
-        <div class="w3-hover-red w3-text-white cfit-border-orange" style="height:320px;background-color: rgb(5.8%, 5.8%, 5.8%); border-radius:10px;">
+    <div class="col-xs-4 hvr hvr-float-shadow cfit-card" style="width:100%;max-width:none;padding:0; display:none;">
+        <div class="w3-text-white cfit-div-dark" style="height:320px; border-radius:10px;">
             <h3 class="container"><b><slot name="playlist_title"></slot></b></h3>
             <div class="container"><slot name="playlist_img"></slot></div>
-            <div class="text-center container-fluid" style="overflow:hidden;height:20%; width:300px;">
+            <div class="text-center container-fluid font-weight-bold" style="overflow:hidden;height:20%; width:300px;">
                 <text><slot name="playlist_descricao"></slot></text>
             </div>
         </div>
