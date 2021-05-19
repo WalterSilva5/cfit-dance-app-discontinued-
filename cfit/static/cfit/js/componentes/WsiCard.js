d8c30daff557217018b8c366d8366c9898c57664
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-19 16:03:03 -0300

    cards alterados

--- cfit/static/cfit/js/componentes/WsiCard.js
+++ cfit/static/cfit/js/componentes/WsiCard.js
@@ -1,11 +1,11 @@
 const WsiCardPlaylist = {
     template: `
-    <div class="col-xs-4 hvr hvr-float-shadow cfit-card" style="width:100%;max-width:none;padding:0; display:none;">
-        <div class="w3-text-white cfit-div-dark" style="height:320px; border-radius:10px;">
-            <h3 class="container"><b><slot name="playlist_title"></slot></b></h3>
+    <div class="col-xs-4" style="width:100%;max-width:none;padding:0;">
+        <div class="cfit-card" style="height:350px; border-radius:3px; display:none;">
+            <h3 class="container text-center" style="text-shadow: 1px 1px 15px #2b2b2b"><b><slot name="playlist_title"></slot></b></h3>
             <div class="container"><slot name="playlist_img"></slot></div>
-            <div class="text-center container-fluid font-weight-bold" style="overflow:hidden;height:20%; width:300px;">
-                <text><slot name="playlist_descricao"></slot></text>
+            <div class="text-center container-fluid font-weight-bold" style="overflow:hidden; font-family:'Lucida Console', monospace,'Courier New';">
+                <h6 style="text-shadow: 0px 1px 5px #2b2b2b"><slot name="playlist_descricao"></slot></h6>
             </div>
         </div>
     </div>
